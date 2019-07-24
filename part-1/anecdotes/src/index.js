ba8944fc7e4003564dcfa921eb8ddb9612c04bd1
lauradjerf <laura.djerf@lianatech.com>
2019-07-24 14:55:54 +0300

    Rename directories

--- part-1/anecdotes/src/index.js
+++ part-1/anecdotes/src/index.js
@@ -1,25 +1,29 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-const Button = ({text, onClick}) => {
-  return <button type="button" onClick={onClick}>{text}</button>;
-}
+const Button = ({ text, onClick }) => (
+  <button type="button" onClick={onClick}>
+    {text}
+  </button>
+);
 
 const App = (props) => {
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [anecdoteVote, setAnecdoteVote] = useState([0, 0, 0, 0, 0, 0]);
 
   const randomize = () => {
     setSelected(Math.floor(Math.random() * 6));
-  }
+  };
 
   return (
     <div>
       {props.anecdotes[selected]}
+      {props.anecdotes[anecdoteVote]}
       <br />
       <Button text="Next anecdote" onClick={randomize} />
     </div>
-  )
-}
+  );
+};
 
 const anecdotes = [
   'If it hurts, do it more often',
@@ -27,10 +31,7 @@ const anecdotes = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+];
 
-ReactDOM.render(
-  <App anecdotes={anecdotes} />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
